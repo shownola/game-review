d39a6a493e9e3358128db2ca127365efeffe6d0f
shownola <swasieko@gmail.com>
2020-12-30 11:33:08 -0700

    Add featured feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_featured
+
+  def  set_featured
+    # Grab genres where featured is set to true
+    @featured = Genre.where(feature_in_navbar: true).order('name ASC')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_154246) do
+ActiveRecord::Schema.define(version: 2020_12_30_175429) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_154246) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "feature_in_navbar"
   end
 
   create_table "users", force: :cascade do |t|
