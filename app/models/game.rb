224ec645df63f87bd9d181269b667eab37027b99
shownola <swasieko@gmail.com>
2020-12-30 15:26:42 -0700

    Implement rich text with ActionText

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,9 @@
 class Game < ApplicationRecord
   has_and_belongs_to_many :genres
   has_one_attached :thumbnail
+  has_rich_text :review
   acts_as_votable
+
 
   def resized_thumbnail
     thumbnail.variant(resize: '240x135!')
