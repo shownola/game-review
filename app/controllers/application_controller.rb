157310c780c4a90d53992a1320313379c3c9e519
shownola <swasieko@gmail.com>
2020-12-30 11:42:39 -0700

    Set admin permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_featured
+  helper_method :is_admin!
 
   def  set_featured
     # Grab genres where featured is set to true
     @featured = Genre.where(feature_in_navbar: true).order('name ASC')
+  end
+
+  def is_admin!
+    unless current_user&.admin
+      redirect_to root_path
+    end
   end
 end
